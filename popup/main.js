6c7274bd0032dca0c2e02b069d17ab192781ab37
healthyCoder <36478181+healthyCoder@users.noreply.github.com>
2021-02-20 10:14:15 +0530

    Added check for highlighted tabs and changed button name Fixes #13

--- popup/main.js
+++ popup/main.js
@@ -2,123 +2,122 @@ import * as warehouse from "./warehouse.js";
 let prevSession = {};
 
 window.onload = () => {
-	document.getElementById("donateImg").addEventListener("click", () => {
-		browser.tabs.create({
-			url: "https://allen505.github.io/save-for-later/contribute"
-		});
-	});
-	
+    document.getElementById("donateImg").addEventListener("click", () => {
+        browser.tabs.create({
+            url: "https://allen505.github.io/save-for-later/contribute"
+        });
+    });
+
 };
 
 let setupAccordion = () => {
-	// setupAccordion() helps initialize all the Accordions in the  HTML document. Must be called every time
-	// a new accordion is made
-	let acc = document.getElementsByClassName("accExpand");
-	let i;
-	// This for-loop is setup so that it runs for all the accordion class nodes found in the HTML document
-	for (i = 0; i < acc.length; i++) {
-		let parent = acc[i].parentElement;
+    // setupAccordion() helps initialize all the Accordions in the  HTML document. Must be called every time
+    // a new accordion is made
+    let acc = document.getElementsByClassName("accExpand");
+    let i;
+    // This for-loop is setup so that it runs for all the accordion class nodes found in the HTML document
+    for (i = 0; i < acc.length; i++) {
+        let parent = acc[i].parentElement;
 
-		acc[i].addEventListener("click", function() {
-			// Toggle between adding and removing the "active" class,
-			// to highlight the button that controls the panel
-			this.classList.toggle("active");
-			// Toggle between hiding and showing the active panel
-			var panel = parent.nextElementSibling;
+        acc[i].addEventListener("click", function() {
+            // Toggle between adding and removing the "active" class,
+            // to highlight the button that controls the panel
+            this.classList.toggle("active");
+            // Toggle between hiding and showing the active panel
+            var panel = parent.nextElementSibling;
 
-			if (panel.style.display === "block") {
-				panel.style.display = "none";
-			} else {
-				panel.style.display = "block";
-			}
-		});
-	}
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
 
-	let accDel = document.getElementsByClassName("accDelete");
-	for (i = 0; i < accDel.length; i++) {
-		let parent = accDel[i].parentElement;
-		accDel[i].addEventListener("click", () => {
-			let delElement = parent.getAttribute("id");
-			warehouse.deleteHandler(delElement).then(() => {
-				updater();
-			});
-		});
-	}
+    let accDel = document.getElementsByClassName("accDelete");
+    for (i = 0; i < accDel.length; i++) {
+        let parent = accDel[i].parentElement;
+        accDel[i].addEventListener("click", () => {
+            let delElement = parent.getAttribute("id");
+            warehouse.deleteHandler(delElement).then(() => {
+                updater();
+            });
+        });
+    }
 };
 
 export function updater() {
-	warehouse.updateHandler().then(data => {
-		let tabsList = document.getElementById("saved-content");
+    warehouse.updateHandler().then(data => {
+        let tabsList = document.getElementById("saved-content");
 
-		tabsList.innerHTML = "";
-		if (data != undefined) {
-			console.log(data);
-			tabsList.appendChild(data);
-			setupAccordion();
-		}
-	});
+        tabsList.innerHTML = "";
+        if (data != undefined) {
+            tabsList.appendChild(data);
+            setupAccordion();
+        }
+    });
 }
 
 function listenForClicks() {
-	// The listenForClicks() is called as soon as HTML page is loaded
-	updater();
+    // The listenForClicks() is called as soon as HTML page is loaded
+    updater();
 
-	// Adding an eventListener so that whenever a click is registered within the pop-up,
-	// the respective condition is called using the if statements written at the end
-	// of the below function
-	document.addEventListener("click", e => {
-		function saveIt() {
-			// The saveIt() is called whenever a node with the class "save" is clicked
-			// saveIt() adds a new Accordion to the pop-up
-			var conditions = { currentWindow: true };
-			let tabsList = document.getElementById("saved-content");
+    // Adding an eventListener so that whenever a click is registered within the pop-up,
+    // the respective condition is called using the if statements written at the end
+    // of the below function
+    document.addEventListener("click", e => {
+        function saveIt() {
+            // The saveIt() is called whenever a node with the class "save" is clicked
+            // saveIt() adds a new Accordion to the pop-up
+            var conditions = { currentWindow: true };
+            let tabsList = document.getElementById("saved-content");
 
-			// conditions is used by the getCurrentWindowTabs() to access tabs only
-			// in the current window
-			// tabsList is used to access the div in which the Accordion is to be added
-			// currentTabs is used to store the Accordion which will be added to the pop-up
+            // conditions is used by the getCurrentWindowTabs() to access tabs only
+            // in the current window
+            // tabsList is used to access the div in which the Accordion is to be added
+            // currentTabs is used to store the Accordion which will be added to the pop-up
 
-			getCurrentWindowTabs().then(tabs => {
-				// getCurrentWindowTabs() is defined below and helps access the tabs which
-				// are present in the given window
+            getCurrentWindowTabs().then(tabs => {
+                // getCurrentWindowTabs() is defined below and helps access the tabs which
+                // are present in the given window
 
-				tabsList.textContent = "";
-				// createList() is called to generate the code for Accordion
-				warehouse.saveHandler(tabs).then(data => {
-					tabsList.appendChild(data);
-					setupAccordion();
-				});
+                tabsList.textContent = "";
+                // createList() is called to generate the code for Accordion
+                warehouse.saveHandler(tabs).then(data => {
+                    tabsList.appendChild(data);
+                    setupAccordion();
+                });
 
-				// currentTabs is updated by the createList() and setupAccordion is called
-				// to initialize the newly added Accordion
-			});
+                // currentTabs is updated by the createList() and setupAccordion is called
+                // to initialize the newly added Accordion
+            });
 
-			function getCurrentWindowTabs() {
-				return browser.tabs.query(conditions);
-			}
-		}
+            function getCurrentWindowTabs() {
+                return browser.tabs.query(conditions);
+            }
+        }
 
-		function reset() {
-			browser.storage.local.clear();
-			document.getElementById("saved-content").innerHTML = "";
-		}
+        function reset() {
+            browser.storage.local.clear();
+            document.getElementById("saved-content").innerHTML = "";
+        }
 
-		function reportError(error) {
-			console.error(`An error occured: ${error}`);
-		}
+        function reportError(error) {
+            console.error(`An error occured: ${error}`);
+        }
 
-		if (e.target.classList.contains("save")) {
-			browser.tabs
-				.query({ active: true, currentWindow: true })
-				.then(saveIt)
-				.catch(reportError);
-		} else if (e.target.classList.contains("reset")) {
-			browser.tabs
-				.query({ active: true, currentWindow: true })
-				.then(reset)
-				.catch(reportError);
-		}
-	});
+        if (e.target.classList.contains("save")) {
+            browser.tabs
+                .query({ active: true, currentWindow: true })
+                .then(saveIt)
+                .catch(reportError);
+        } else if (e.target.classList.contains("reset")) {
+            browser.tabs
+                .query({ active: true, currentWindow: true })
+                .then(reset)
+                .catch(reportError);
+        }
+    });
 }
 
 document.addEventListener("DOMContentLoaded", listenForClicks);
--- popup/warehouse.js
+++ popup/warehouse.js
@@ -7,207 +7,225 @@ let pattern = /^(http|https|file):\/\//;
 // The pattern helps catch only http/https/file links excluding any about/ftp/ws/wss links
 
 class storageObject {
-	constructor(id, title, tabs, tags) {
-		this.id = id;
-		this.title = title;
-		this.tabs = tabs;
-		this.pinned=false;
-		this.tags = tags;
-	}
+    constructor(id, title, tabs, tags) {
+        this.id = id;
+        this.title = title;
+        this.tabs = tabs;
+        this.pinned = false;
+        this.tags = tags;
+    }
+}
+
+function countHighlighted(tabs) {
+    let count = 0;
+    for (let tab of tabs) {
+        if (tab.highlighted == true) {
+            count++;
+        }
+    }
+    return count;
+}
+
+function isHighlighted(tab, _, __) {
+    return tab.highlighted;
 }
 
 function createList(storeObj) {
-	// createList() is used to create the Accordion which will be added to the
-	// pop-up. It updates the currentTabs variable to store the newly created
-	// Accordion.
+    // createList() is used to create the Accordion which will be added to the
+    // pop-up. It updates the currentTabs variable to store the newly created
+    // Accordion.
 
-	// id will be the unique identifier of each element in the stored Array
-	let currentTabs = document.createDocumentFragment();
-	let idArray = new Array();
-	for (let id in storeObj) {
-		idArray.unshift(id);
-	}
+    // id will be the unique identifier of each element in the stored Array
+    let currentTabs = document.createDocumentFragment();
+    let idArray = new Array();
+    for (let id in storeObj) {
+        idArray.unshift(id);
+    }
 
-	idArray.forEach(id => {
-		if (storeObj.hasOwnProperty(id)) {
-			// Create the accordion button and set the values of the button
-			let obj = storeObj[id];
+    idArray.forEach(id => {
+        if (storeObj.hasOwnProperty(id)) {
+            // Create the accordion button and set the values of the button
+            let obj = storeObj[id];
 
-			let accDiv = document.createElement("div");
-			let accButton = document.createElement("button");
-			let expandImg = document.createElement("img");
-			let accExpand = document.createElement("button");
-			let delImg = document.createElement("img");
-			let accDelete = document.createElement("button");
+            let accDiv = document.createElement("div");
+            let accButton = document.createElement("button");
+            let expandImg = document.createElement("img");
+            let accExpand = document.createElement("button");
+            let delImg = document.createElement("img");
+            let accDelete = document.createElement("button");
 
-			accButton.textContent = obj.title;
-			accButton.setAttribute("class", "btn btn-link accButton");
-			accButton.addEventListener("click", () => {
-				let windowObj = new Object();
-				let urlArray = new Array();
-				for (let tab of obj.tabs) {
-					urlArray.push(tab.url.toString());
-				}
-				windowObj.url = urlArray;
-				browser.windows.create(windowObj)
-				// deleteHandler(obj.id.toString()).then(() => {
-				// 	mainjs.updater();
-				// });
-			});
+            accButton.textContent = obj.title;
+            accButton.setAttribute("class", "btn btn-link accButton");
+            accButton.addEventListener("click", () => {
+                let windowObj = new Object();
+                let urlArray = new Array();
+                for (let tab of obj.tabs) {
+                    urlArray.push(tab.url.toString());
+                }
+                windowObj.url = urlArray;
+                browser.windows.create(windowObj)
+                    // deleteHandler(obj.id.toString()).then(() => {
+                    // 	mainjs.updater();
+                    // });
+            });
 
-			expandImg.setAttribute("src", "./../icons/drop_down.png");
-			expandImg.setAttribute("class", "icon");
+            expandImg.setAttribute("src", "./../icons/drop_down.png");
+            expandImg.setAttribute("class", "icon");
 
-			accExpand.setAttribute("class", "btn btn-link accExpand");
-			accExpand.appendChild(expandImg);
+            accExpand.setAttribute("class", "btn btn-link accExpand");
+            accExpand.appendChild(expandImg);
 
-			delImg.setAttribute("src", "./../icons/delete.svg");
-			delImg.setAttribute("class", "icon");
-			
-			accDelete.setAttribute("class", "btn btn-link accDelete");
-			accDelete.appendChild(delImg);
+            delImg.setAttribute("src", "./../icons/delete.svg");
+            delImg.setAttribute("class", "icon");
 
-			accDiv.setAttribute("class", "accDiv");
-			accDiv.setAttribute("id", obj.id);
+            accDelete.setAttribute("class", "btn btn-link accDelete");
+            accDelete.appendChild(delImg);
 
-			let panelDiv = document.createElement("div");
-			panelDiv.setAttribute("class", "panel");
+            accDiv.setAttribute("class", "accDiv");
+            accDiv.setAttribute("id", obj.id);
 
-			accDiv.appendChild(accButton);
-			accDiv.appendChild(accExpand);
-			accDiv.appendChild(accDelete);
+            let panelDiv = document.createElement("div");
+            panelDiv.setAttribute("class", "panel");
 
-			// The for loop is run to go through each tab present in the current window
-			for (let tab of obj.tabs) {
-				// Create the li tag into which the link is inserted
-				let listItem = document.createElement("li");
+            accDiv.appendChild(accButton);
+            accDiv.appendChild(accExpand);
+            accDiv.appendChild(accDelete);
 
-				// Create a link for each tab which is saved and set its respective parameters
-				let tabLink = document.createElement("button");
-				let cutoffLength=30;
+            // The for loop is run to go through each tab present in the current window
+            for (let tab of obj.tabs) {
+                // Create the li tag into which the link is inserted
+                let listItem = document.createElement("li");
 
-				if (tab.textContent.length >= cutoffLength)
-					tabLink.textContent =
-						tab.textContent.slice(0, cutoffLength) + "...";
-				else tabLink.textContent = tab.textContent;
-				tabLink.setAttribute("class", "btn btn-link accButton");
-				listItem.addEventListener("click", () => {
-					browser.tabs.create({
-						url:tab.url
-					})
-				})
+                // Create a link for each tab which is saved and set its respective parameters
+                let tabLink = document.createElement("button");
+                let cutoffLength = 30;
 
-				listItem.appendChild(tabLink);
-				panelDiv.appendChild(listItem);
-			}
-			currentTabs.appendChild(accDiv);
-			currentTabs.appendChild(panelDiv);
-		}
-	});
-	return currentTabs;
+                if (tab.textContent.length >= cutoffLength)
+                    tabLink.textContent =
+                    tab.textContent.slice(0, cutoffLength) + "...";
+                else tabLink.textContent = tab.textContent;
+                tabLink.setAttribute("class", "btn btn-link accButton");
+                listItem.addEventListener("click", () => {
+                    browser.tabs.create({
+                        url: tab.url
+                    })
+                })
+
+                listItem.appendChild(tabLink);
+                panelDiv.appendChild(listItem);
+            }
+            currentTabs.appendChild(accDiv);
+            currentTabs.appendChild(panelDiv);
+        }
+    });
+    return currentTabs;
 }
 
 async function storeIt(tabs) {
-	// storeIt takes only the required data from what is provided by the browser and
-	// stores it into the storage.local
+    // storeIt takes only the required data from what is provided by the browser and
+    // stores it into the storage.local
 
-	// dataToStore is the object which contains data of the window which is going to be saved
+    // dataToStore is the object which contains data of the window which is going to be saved
 
-	let time = new Date();
-	var id = time.getTime();
-	let tabArray = new Array();
+    let time = new Date();
+    var id = time.getTime();
+    let tabArray = new Array();
 
-	for (let tab of tabs) {
-		if (tab.url.match(pattern) != null) {
-			let newTab = {};
-			newTab.textContent = tab.title || tab.url;
-			newTab.url = tab.url;
-			newTab.iconUrl = tab.favIconUrl || null;
-			tabArray.push(newTab);
-		}
-	}
-	let title =
-		time.getDate() +
-		"/" +
-		(time.getMonth() + 1) +
-		" " +
-		time.getHours() +
-		":" +
-		time.getMinutes() +
-		" | " +
-		tabArray.length +
-		" tabs";
-	let tags = null;
+    if (countHighlighted(tabs) > 1) {
+        tabs = tabs.filter(isHighlighted);
+    }
 
-	var dataToStore = new storageObject(id, title, tabArray, tags);
-	var obj = {};
-	obj[id] = dataToStore;
+    for (let tab of tabs) {
+        if (tab.url.match(pattern) != null) {
+            let newTab = {};
+            newTab.textContent = tab.title || tab.url;
+            newTab.url = tab.url;
+            newTab.iconUrl = tab.favIconUrl || null;
+            tabArray.push(newTab);
+        }
+    }
+    let title =
+        time.getDate() +
+        "/" +
+        (time.getMonth() + 1) +
+        " " +
+        time.getHours() +
+        ":" +
+        time.getMinutes() +
+        " | " +
+        tabArray.length +
+        " tabs";
+    let tags = null;
 
-	browser.storage.local
-		.set(obj)
-		.then(() => {})
-		.catch(error => {
-			console.log("Storage error occured: " + error);
-		});
+    var dataToStore = new storageObject(id, title, tabArray, tags);
+    var obj = {};
+    obj[id] = dataToStore;
 
-	// });
+    browser.storage.local
+        .set(obj)
+        .then(() => {})
+        .catch(error => {
+            console.warn("Storage error occured: " + error);
+        });
+
+    // });
 }
 
 function retrieveIt() {
-	// This function retrieves data from the storage.local and returns a Promise
-	return browser.storage.local.get(null);
+    // This function retrieves data from the storage.local and returns a Promise
+    return browser.storage.local.get(null);
 }
 
 export async function saveHandler(tabs) {
-	// Handles all the function calls related to saving a session
+    // Handles all the function calls related to saving a session
 
-	let returnPromise = new Promise((resolve, reject) => {
-		storeIt(tabs)
-			.then(() => {
-				retrieveIt().then(obj => {
-					let returnTab = createList(obj);
-					resolve(returnTab);
-				});
-			})
-			.catch(error => {
-				console.log("Error occured while saving: " + error);
-				reject(error);
-			});
-	});
-	return await returnPromise;
+    let returnPromise = new Promise((resolve, reject) => {
+        storeIt(tabs)
+            .then(() => {
+                retrieveIt().then(obj => {
+                    let returnTab = createList(obj);
+                    resolve(returnTab);
+                });
+            })
+            .catch(error => {
+                console.log("Error occured while saving: " + error);
+                reject(error);
+            });
+    });
+    return await returnPromise;
 }
 
 export async function updateHandler() {
-	// updateHandler syncs the data in storage.local with data displayed
-	// in the popup
+    // updateHandler syncs the data in storage.local with data displayed
+    // in the popup
 
-	// counter()
-	let returnPromise = new Promise((resolve, reject) => {
-		retrieveIt()
-			.then(obj => {
-				if (obj != undefined) {
-					let returnTab = createList(obj);
-					resolve(returnTab);
-				} else {
-					resolve(undefined);
-				}
-			})
-			.catch(error => {
-				console.log("Error occured while updating: " + error);
-				reject(error);
-			});
-	});
-	return await returnPromise;
+    // counter()
+    let returnPromise = new Promise((resolve, reject) => {
+        retrieveIt()
+            .then(obj => {
+                if (obj != undefined) {
+                    let returnTab = createList(obj);
+                    resolve(returnTab);
+                } else {
+                    resolve(undefined);
+                }
+            })
+            .catch(error => {
+                console.warn("Error occured while updating: " + error);
+                reject(error);
+            });
+    });
+    return await returnPromise;
 }
 
 export async function deleteHandler(key) {
-	let returnPromise = new Promise((resolve, reject) => {
-		let returnValue = browser.storage.local.remove(key);
-		if (returnValue === undefined) {
-			reject("Error occured while deleting");
-		} else {
-			resolve("Deleted successfully");
-		}
-	});
-	return await returnPromise;
+    let returnPromise = new Promise((resolve, reject) => {
+        let returnValue = browser.storage.local.remove(key);
+        if (returnValue === undefined) {
+            reject("Error occured while deleting");
+        } else {
+            resolve("Deleted successfully");
+        }
+    });
+    return await returnPromise;
 }
