c2c11732ab2c06611106c860d62ccd45ca17aca4
healthyCoder <36478181+healthyCoder@users.noreply.github.com>
2023-02-19 12:31:36 +0530

    Change BG color of Badge and remove it for No saves

--- background-script.js
+++ background-script.js
@@ -1,35 +1,34 @@
 async function counter() {
-	// This function retrieves data from the storage.local and returns a Promise
-	return browser.storage.local.get(null);
+  // This function retrieves data from the storage.local and returns a Promise
+  return browser.storage.local.get(null);
 }
 
 async function setBadge() {
-	while (1) {
-		counter().then(obj => {
-			let count = 0;
-			for (i in obj) {
-				count++;
-			}
-			if (count == 0) {
-				browser.browserAction.setBadgeBackgroundColor({
-					color: "#393939"
-				});
-			} else {
-				browser.browserAction.setBadgeBackgroundColor({
-					color: "#FF0E46"
-				});
-			}
-
-			browser.browserAction.setBadgeText({
-				text: count.toString()
-			});
-		});
-		await sleep(500);
-	}
+  while (1) {
+    counter().then(obj => {
+      let count = 0;
+      for (i in obj) {
+        count++;
+      }
+      if (count == 0) {
+        browser.browserAction.setBadgeText({
+          text: ""
+        });
+      } else {
+        browser.browserAction.setBadgeBackgroundColor({
+          color: "#c5e7ff"
+        });
+        browser.browserAction.setBadgeText({
+          text: count.toString()
+        });
+      }
+    });
+    await sleep(500);
+  }
 }
 
 function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 setBadge();
